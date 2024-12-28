ffc19752b8068b170f119d598bdcc7a4d94895d6
Ayuniiieee02 <ayunie.rastro2324@gmail.com>
2024-12-29 03:11:17 +0800

    Create keywords.py

---
+++ keywords.py
@@ -0,0 +1,44 @@
+# keywords.py
+
+it_keywords = ['c++', 'java', 'python', 'CSS', 'HTML', 'Programming', 'Sains Komputer', 
+               'information technology', 'it','Bachelor in Information Technology in Education',
+               'ICT','Cloud Computing','PHP','sql']
+
+software_keywords = ['Machine Learning','Software Engineering', 'Flask','Bachelor in Software Engineering',
+                     'TensorFlow','Frontend Development','Agile Development','Full-Stack Development', 
+                     'Artificial Intelligence','Object-Oriented Programming']
+
+multimedia_keywords = ['Bachelor in Multimedia','Adobe Photoshop','Graphic Design','Animation','Unity',
+                       'Storyboarding','Augmented Reality (AR)','Character Design','Digital Arts',
+                       'Cinematography', 'Multimedia']
+
+science_keywords = ['Bachelor in Science','Science Teacher','Science Clubs','Standard 1-6 Science',
+                    'Biology','Physics','Chemistry','Pure Science','Chemical Reactions','Lab Work' ]
+
+math_keywords = ['Bachelor of Mathematics','Calculus','Math Olympiad','Math Games','Statistics',
+                 'Numeracy Skills','Math Teacher','Form 1-5 Math','Functions','Math Tutoring']
+
+pk_keyword = ['Pendidikan Khas','Sign Language','Special Education','Autisme', 'Dyslexia', 
+              'ADHD', 'Speech Delay','Teaching Special Needs Students',
+              'Program Pendidikan Khas Integrasi (PPKI)','Braille']
+
+pakk_keywords = ['Early Childhood Education','Play-Based Learning','Child Safety and Protection',
+                 'Health and Nutrition for Children','Exploratory Learning','Sensory Play',
+                 'Teaching Preschoolers','Kindergarten Teacher','Montessori','Creative Arts for Children']
+
+sj_keywords = ['Bachelor in History','History Teacher','Artifact Analysis','Archaeology Basics',
+               'Historical Essay Writing','Asian History', 'European History', 
+                'American History', 'Middle Eastern History', 'African History','History of Religion', 
+                'History of Science', 'Postcolonial Studies']
+
+geo_keywords = ['Bachelor in Geography','Geography Teacher''Natural Disasters', 'Environmental Studies',
+                'Urbanization', 'Population Studies', 'Biodiversity', 'Water Resources', 
+                'Agriculture Geography', 'Tourism Geography', 'Landforms', 'GPS Tools', 
+                'GIS Software', 'Survey Equipment''Data Visualization', 'Remote Sensing Techniques',
+                'Satellite Imagery Interpretation', 'Spatial Thinking']
+
+eco_keywords = ['Bachelor in Economics','Health Economics', 'Environmental Economics',
+                'Public Economics', 'Urban Economics','Economics Teacher', 'Economics Educator',
+                'Data Analysis', 'Market Research', 'Policy Analysis', 'Statistical Analysis', 
+                'Economic Forecasting', 'Supply and Demand Analysis', 'Cost-Benefit Analysis',]
+
