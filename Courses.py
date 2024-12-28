3f6506aed4ac1ae43653077b9848fa9838cd9daa
Ayuniiieee02 <ayunie.rastro2324@gmail.com>
2024-12-29 02:39:02 +0800

    Create Courses.py

---
+++ Courses.py
@@ -0,0 +1,9 @@
+resume_videos = ['https://www.youtube.com/watch?v=Tt08KmFfIYQ','https://www.youtube.com/watch?v=aD7fP-2u3iY',
+                 'https://www.youtube.com/watch?v=zVHnRaQE7Oc','https://www.youtube.com/watch?v=lBptzB5W2N8',
+                 'https://www.youtube.com/watch?v=iLI7fb0FalU','https://www.youtube.com/watch?v=R3abknwWX7k',
+                 'https://www.youtube.com/watch?v=u75hUSShvnc','https://www.youtube.com/watch?v=uuxgg0SSjj0']
+
+interview_videos = ['https://www.youtube.com/watch?v=4VkZaNZGkv4','https://www.youtube.com/watch?v=QLdpzR4l7Sc',
+                    'https://www.youtube.com/watch?v=QkPRpz56aEg','https://www.youtube.com/watch?v=qpkegRmPgis',
+                    'https://www.youtube.com/watch?v=xSqGs2YLM8k','https://www.youtube.com/watch?v=lk1GH8AnqCg'
+                    'https://www.youtube.com/watch?v=4wBy58qk7Bg','https://m.youtube.com/watch?v=OU6aUAX-qz0&t=390s']
